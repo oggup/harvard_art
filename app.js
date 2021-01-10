50bfa878f7a1bebf76c9c0f71a13301fd008620b
Oggup <edifoo@gmail.com>
2021-01-10 04:02:06 -0800

    completed module 2 and most of module 3, added preview rendering as well as information and images when an particular object is selected

--- app.js
+++ app.js
@@ -1,157 +1,218 @@
 const BASE_URL = 'https://api.harvardartmuseums.org';
-const KEY = 'apikey=07dffc48-e68d-40b5-8ec9-cf4128637d97'; // USE YOUR KEY HERE
+const KEY = 'apikey=07dffc48-e68d-40b5-8ec9-cf4128637d97';
 
-async function fetchObjects() {
-    const url = `${ BASE_URL }/object?${ KEY }`;
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;  
-      console.log(data);
-
-    } catch (error) {
-      console.error("whoops");
-    };
+function onFetchStart() {
+  $('#loading').addClass('active');
 };
 
+function onFetchEnd() {
+  $('#loading').removeClass('active');
+};
+
+async function fetchObjects() {
+  const url = `${ BASE_URL }/object?${ KEY }`;
+  onFetchStart();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;  
+    console.log(data);
+
+  } catch (error) {
+    console.error("whoops");
+  }finally {
+    onFetchEnd();
+  };
+};
 
 async function fetchAllCenturies() {
-    const url = `${ BASE_URL }/century?${ KEY }&size=100&sort=temporalorder`;
-    if (localStorage.getItem('centuries')) {
-        return JSON.parse(localStorage.getItem('centuries'));
-    };
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const records = data.records;
-      localStorage.setItem('centuries',json.stringify(records));
-
-        
-      return records;
-    } catch (error) {
-      console.error(error);
-    };
+  const url = `${ BASE_URL }/century?${ KEY }&size=100&sort=temporalorder`;
+  if (localStorage.getItem('centuries')) {
+      return JSON.parse(localStorage.getItem('centuries'));
+  };
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const records = data.records;
+    localStorage.setItem('centuries',JSON.stringify(records));
+    return records;
+  } catch (error) {
+    console.error(error);
+  };
 };
 
 async function fetchAllClassifications() {
-    const url = `${ BASE_URL }/classification?${ KEY }&size=100&sort=name`;
-    if (localStorage.getItem('classifications')) {
-        return JSON.parse(localStorage.getItem('classifications'));
-    };
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const records = data.records;
-      localStorage.setItem('classifications', json.stringfy(records));
-
-        
-      return records;
-    } catch (error) {
+  const url = `${ BASE_URL }/classification?${ KEY }&size=100&sort=name`;
+  onFetchStart();
+  if (localStorage.getItem('classifications')) {
+      return JSON.parse(localStorage.getItem('classifications'));
+  };  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const records = data.records;
+    localStorage.setItem('classifications', JSON.stringfy(records));
+    return records;
+  } catch (error) {
       console.error(error);
-    };
+  } finally {
+      onFetchEnd();
+  };
 };
 
-async function prefetchCategoryLists() {
-    try {
-      const [classifications, centuries] = await Promise.all([fetchAllClassifications(), fetchAllCenturies()]);
-        // This provides a clue to the user, that there are items in the dropdown
-        $('.classification-count').text(`(${ classifications.length })`);
-
-        classifications.forEach(classification => {
-        // append a correctly formatted option tag into
-        // the element with id select-classification
-        $('#select-classification').append($(`<option value="value text">${classification.name}</option>`));
-        });
-  
-        // This provides a clue to the user, that there are items in the dropdown
-        $('.century-count').text(`(${ centuries.length }))`);
-  
-        centuries.forEach(century => {
-         // append a correctly formatted option tag into
-         // the element with id select-century
-         $('#select-century').append($(`<option value="value text">${century.name}</option>`));
-        });
-   
-      
-    } catch (error) {
+async function prefetchCategoryLists() { // reordered boiler plate module 1
+  try {
+    const [classifications, centuries] = await Promise.all([
+       fetchAllClassifications(),
+       fetchAllCenturies()
+      ]);
+     
+      $('.classification-count').text(`(${classifications.length})`);
+      classifications.forEach(classification => {
+          $('#select-classification').append($(`<option value="${classification.name}">${classification.name}</option>`));
+      });
+      $('.century-count').text(`(${centuries.length})`);
+      centuries.forEach(century => {          
+          $('#select-century').append($(`<option value="${century.name}">${century.name}</option>`));
+      });
+  } catch (error) {
       console.error(error);
-    };
+  };
 };
 
-function buildSearchString(){
-    const rootUrl = `https://api.harvardartmuseums.org/object?${KEY}`
-    const terms = [... $('#search select')].map( function (element){
-    return `${$(`element`).attr('name')}=${$(`element`).val()}`;
-    }).join('&');
-    const keywords = `keyword=${$('#keywords').val()}`
-    return `${rootUrl}&${terms}&${keywords}`;
-
-};
-
-function onFetchStart() {
-    $('#loading').addClass('active');
-};
-  
-function onFetchEnd() {
-    $('#loading').removeClass('active');
-};
+function buildSearchString() {
+  const selectClass = $('#select-classification').val()
+  const selectCentury = $('#select-century').val()
+  const keyWord = $('#keywords').val()
+  const url = `${BASE_URL}/object?${KEY}&classification=${selectClass}&century=${selectCentury}&keyword=${keyWord}`;
+  return url;
+}
 
 function renderPreview(record) {
-  // grab description, primaryimageurl, and title from the record
   const { description, primaryimageurl, title} = record;
-  /*
-  Template looks like this:
-  */
   return $(`<div class="object-preview">
     <a href="#">
-      <img src="${primaryimageurl}" />
-      <h3>${title}</h3>
-      <h3>${description}</h3>
+      ${primaryimageurl ? `<img src="${primaryimageurl}" />` : ""}
+      ${title ? `<h3>${title}</h3>` : ""}
+      ${description ? `<h3>${description}</h3>` : ""}
     </a>
   </div>`).data('record', record);
 };
-  /*
-  Some of the items might be undefined, if so... don't render them
-  With the record attached as data, with key 'record'
-  */
 
-  // return new element
-function updatePreview(records) {
+function updatePreview(records, info) {
   const root = $('#preview');
-  const resultsEl = root.find('.results');
-  resultsEl.empty();
-  resultsEl.append(records.map(renderPreview));
-
-
-
-  // grab the results element, it matches .results inside root
-  // empty it
-  // loop over the records, and append the renderPreview
+  if (info.next) {
+      $('.next').data('url', info.next).attr('disabled', false)
+  } else {
+      $('.next').data('url', null).attr('disabled', true)
+  };
+  if (info.prev) {
+      $('.prev').data('url', info.prev).attr('disabled', false)
+  } else {
+      $('.prev').data('url', null).attr('disabled', true)
+  };
+  const results = root.find('.results');
+  results.empty();
+  results.append(records.map(renderPreview));
 };
 
+function photosHTML(images, primaryimageurl) {
+  if (images && images.length > 0 ){
+    return images.map(function(image){
+      return `<img src="${ image.baseimageurl }" />`});
+
+  } else if ( primaryimageurl){
+    return `<img src = "${primaryimageurl}"/>`;  
+  } else {
+    return "";
+  };
+};
 
 $('#search').on('submit', async function (event) {
-    // prevent the default
-    event.preventDefault();
-    onFetchStart();    
-    try {
-      // get the url from `buildSearchString`
-      // fetch it with await, store the result
-      // log out both info and records when you get them
-        const response = await fetch(buildSearchString());
-        const {info, records} = await response.json();
-        updatePreview(records);
-        console.log(info);
-        console.log(records);
-    } catch (error) {
-      // log out the error
-    } finally {
-        onFetchEnd();
-    }
+  event.preventDefault();
+  onFetchStart();
+  try {
+      const encodeUrl = encodeURI(buildSearchString())
+      const response = await fetch(encodeUrl);
+      const {info, records} = await response.json();
+      console.log(info, records)
+      updatePreview(records, info);
+  } catch (error) {
+      console.error(error);
+  } finally {
+      onFetchEnd();
+  };
 });
+
+$('#preview .next, #preview .previous').on('click', async function () {
+  onFetchStart();
+  try {
+      let url = $(this).data('url')
+      const response = await fetch(url);
+      const {info, records }= await response.json();
+      updatePreview(records, info);
+  } catch (error) {
+      console.error(error);
+  } finally {
+      onFetchEnd();
+  };        
+});
+
+$('#preview').on('click', '.object-preview', function (event) {
+  event.preventDefault();
+  const objectPreview = $(this).closest('.object-preview');
+  const objectPreviewData = objectPreview.data('record');
+  console.log(objectPreviewData);
+  $('#feature').html(renderFeature(objectPreviewData));
+});
+
+function renderFeature(record) {
+  const {title, dated, description, culture, style, technique, medium, dimensions, people, department, division, contact, creditline ,images, primaryimageurl} = record;
+ return $(` <div class="object-feature">
+        <header>
+        <h3>${ title }<h3>
+        <h4>${ dated }</h4>
+        </header>
+        <section class="facts">
+        ${ factHTML("Description", description,) }
+        ${ factHTML("Culture", culture, 'culture') }
+        ${ factHTML("Technique", technique, 'technique') }
+        ${ factHTML("Medium", medium, 'medium') }
+        ${ factHTML("Dimensions", dimensions) }
+        ${record.people ? record.people.map(function(person) {
+              return factHTML('Person', person.displayname, 'person');
+              }).join(''):''}
+        ${ factHTML("Department", department) }
+        ${ factHTML("Division", division) }
+        ${ factHTML('Contact', `<a target="_blank" href="mailto:${ contact }">${ contact }</a>`) }
+        ${ factHTML("Credit Line", creditline) }
+        </section>
+        <section class="photos">
+        ${ photosHTML(record.images, record.primaryimageurl) }
+        </section>
+        </div>`);
+};
+
+function searchURL(searchType, searchString) {
+    return `${ BASE_URL }/object?${ KEY }&${ searchType}=${ searchString }`;
+};
+  
+function factHTML(title, content, searchTerm = null) {
+    // if content is empty or undefined, return an empty string ''
+  
+    // otherwise, if there is no searchTerm, return the two spans
+  
+    // otherwise, return the two spans, with the content wrapped in an anchor tag
+    if (!content){
+      return ""
+    }
+    return `
+    ${content ? `<span class="title">${title}</span>`: ""}
+    ${content ? `<span class="content"><a href=${searchURL(content, searchTerm)}>${ content}</a></span>` : "" }
+ `;
+  };
+
+
 
 function bootstrap(){
     fetchObjects();
